4e84714572cd015af5f775ae5df69336297a70f0
Ajeibi Oche Francis <ajeibipikin@gmail.com>
2025-02-07 12:09:59 +0100

    commit

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const classifyRoutes = require('./routes/classify');
+const classifyRoutes = require("./routes/classify");
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
-
-app.options('*', cors());
+app.options("*", cors());
 
 app.use(express.json());
-app.use ('/api', classifyRoutes);
-
-
+app.use("/api", classifyRoutes);
 
 module.exports = app;
